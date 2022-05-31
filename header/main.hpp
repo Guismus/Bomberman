547122747e5d2e95c5234f77993d0b6ddf03952d
zebi <hugo.gardes@epitech.eu>
2022-05-31 06:37:03 -0700

    test cmake

---
+++ header/main.hpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-MPL-4-1-indiestudio-paul.comte
+** File description:
+** main.hpp
+*/
+
+#ifndef __main_H__
+    #define __main_H__
+    #include <iostream>
+    #include "raylib.h"
+
+    #define Height 1080
+    #define Width 19200
+#endif
---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2021
+** B-YEP-400-MPL-4-1-indiestudio-paul.comte
+** File description:
+** main.cpp
+*/
+
+#include "main.hpp"
+
+int main()
+{
+    
+    return (0);
+}
