484fb99cc65f2050d625f58a0828b6acacab51e7
Guismus <guillaume.boudaille@epitech.eu>
2022-06-15 17:05:30 +0200

    Error push useless files

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/guismus/Documents/B-YEP-400-MPL-4-1-indiestudio-paul.comte/build/_deps/raylib-src
-# Build directory: /home/guismus/Documents/B-YEP-400-MPL-4-1-indiestudio-paul.comte/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
